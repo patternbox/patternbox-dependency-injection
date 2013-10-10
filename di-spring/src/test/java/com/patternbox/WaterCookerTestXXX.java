05f134338bc97ee938771e364a236e2b294ae25a
patternbox <ehms@patternbox.com>
2013-10-10 17:35:39 +0200

    Refactoring Netbeans

---
+++ di-spring/src/test/java/com/patternbox/WaterCookerTestXXX.java
@@ -0,0 +1,34 @@
+package com.patternbox;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class WaterCookerTestXXX extends TestCase {
+
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public WaterCookerTestXXX(String testName) {
+    super(testName);
+  }
+
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite() {
+    return new TestSuite(WaterCookerTestXXX.class);
+  }
+
+  /**
+   * Rigourous Test :-)
+   */
+  public void testApp() {
+    assertTrue(true);
+  }
+}
