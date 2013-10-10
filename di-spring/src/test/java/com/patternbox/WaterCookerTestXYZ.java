498f083152912ddb9e47582be11a00406dde8fb3
patternbox <ehms@patternbox.com>
2013-10-10 17:37:18 +0200

    Refactoring Eclipse (+rename)

--- di-spring/src/test/java/com/patternbox/WaterCookerTestXYZ.java
+++ di-spring/src/test/java/com/patternbox/WaterCookerTestXYZ.java
@@ -7,14 +7,14 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class WaterCookerTestXXX extends TestCase {
+public class WaterCookerTestXYZ extends TestCase {
 
   /**
    * Create the test case
    *
    * @param testName name of the test case
    */
-  public WaterCookerTestXXX(String testName) {
+  public WaterCookerTestXYZ(String testName) {
     super(testName);
   }
 
@@ -22,7 +22,7 @@ public class WaterCookerTestXXX extends TestCase {
    * @return the suite of tests being tested
    */
   public static Test suite() {
-    return new TestSuite(WaterCookerTestXXX.class);
+    return new TestSuite(WaterCookerTestXYZ.class);
   }
 
   /**
